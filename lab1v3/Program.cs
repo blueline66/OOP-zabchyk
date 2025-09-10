f9ffd6d3766c94299c8adfe2465a6de64eac1ae6
blueline66 <darinazabcik980@gmail.com>
2025-09-10 19:01:36 +0300

    ..

---
+++ lab1v3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Student
+{
+    private string name;
+    private int id;
+
+    public double AverageMark { get; set; }
+
+    public Student(string name, int id, double averageMark)
+    {
+        this.name = name;
+        this.id = id;
+        this.AverageMark = averageMark;
+    }
+
+    ~Student()
+    {
+        Console.WriteLine($"Об'єкт студента {name} з id {id} знищено.");
+    }
+
+    public void PrintCard()
+    {
+        Console.WriteLine($"Студент: {name}, ID: {id}, Середній бал: {AverageMark}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student("Іван", 101, 85.5);
+        Student s2 = new Student("Олена", 102, 92.3);
+        Student s3 = new Student("Петро", 103, 77.8);
+
+        s1.PrintCard();
+        s2.PrintCard();
+        s3.PrintCard();
+    }
+}
