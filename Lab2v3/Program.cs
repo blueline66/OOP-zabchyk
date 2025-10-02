ecc7f878d3a60aa3aa1431882a91534b7c408603
blueline66 <darinazabcik980@gmail.com>
2025-10-02 14:32:33 +0300

    Lab2v8

--- Lab2v3/Program.cs
+++ Lab2v3/Program.cs
@@ -2,40 +2,115 @@
 
 namespace Lab2
 {
-    class Program
+    using System;
+
+public class Vector3D
+{
+    private double x;
+    private double y;
+    private double z;
+
+    public double X
     {
-        static void Main(string[] args)
+        get => x;
+        set
         {
-            try
-            {
-                Vector3D v1 = new Vector3D(3, 4, 5);
-                Vector3D v2 = new Vector3D(1, 2, 3);
-
-                Console.WriteLine("Вектор v1: " + v1);
-                Console.WriteLine("Вектор v2: " + v2);
-
-                Vector3D sum = v1 + v2;
-                Vector3D diff = v1 - v2;
-
-                Console.WriteLine("v1 + v2 = " + sum);
-                Console.WriteLine("v1 - v2 = " + diff);
-
-                Console.WriteLine("v1 == v2 ? " + (v1 == v2));
-                Console.WriteLine("v1 != v2 ? " + (v1 != v2));
-
-                Console.WriteLine("v1[0] = " + v1[0]);
-                Console.WriteLine("v1[1] = " + v1[1]);
-                Console.WriteLine("v1[2] = " + v1[2]);
-
-                v1[0] = 10;
-                Console.WriteLine("v1 після зміни X через індексатор: " + v1);
-
-                Vector3D bigVector = new Vector3D(100, 100, 100);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Помилка: " + ex.Message);
-            }
+            if (IsValidLength(value, y, z))
+                x = value;
+            else
+                throw new ArgumentException("Довжина вектора не може перевищувати 100!");
         }
     }
+
+    public double Y
+    {
+        get => y;
+        set
+        {
+            if (IsValidLength(x, value, z))
+                y = value;
+            else
+                throw new ArgumentException("Довжина вектора не може перевищувати 100!");
+        }
+    }
+
+    public double Z
+    {
+        get => z;
+        set
+        {
+            if (IsValidLength(x, y, value))
+                z = value;
+            else
+                throw new ArgumentException("Довжина вектора не може перевищувати 100!");
+        }
+    }
+
+    // Конструктор
+    public Vector3D(double x, double y, double z)
+    {
+        if (!IsValidLength(x, y, z))
+            throw new ArgumentException("Довжина вектора не може перевищувати 100!");
+        
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Довжина вектора
+    public double Length => Math.Sqrt(x * x + y * y + z * z);
+
+    private static bool IsValidLength(double x, double y, double z)
+    {
+        double length = Math.Sqrt(x * x + y * y + z * z);
+        return length <= 100;
+    }
+
+    // Оператори
+    public static Vector3D operator +(Vector3D a, Vector3D b)
+        => new Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+
+    public static Vector3D operator -(Vector3D a, Vector3D b)
+        => new Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+
+    public static bool operator ==(Vector3D a, Vector3D b)
+        => a.x == b.x && a.y == b.y && a.z == b.z;
+
+    public static bool operator !=(Vector3D a, Vector3D b)
+        => !(a == b);
+
+    // Перевизначення Equals та GetHashCode
+    public override bool Equals(object? obj)
+    {
+        if (obj is Vector3D other)
+            return this == other;
+        return false;
+    }
+
+    public override int GetHashCode()
+        => (x, y, z).GetHashCode();
+
+    // Для зручного виводу
+    public override string ToString()
+        => $"({x}, {y}, {z}), |V| = {Length:F2}";
+}
+
+// Приклад використання
+class Program
+{
+    static void Main()
+    {
+        Vector3D v1 = new Vector3D(3, 4, 5);
+        Vector3D v2 = new Vector3D(1, 2, 3);
+
+        Console.WriteLine("Вектор 1: " + v1);
+        Console.WriteLine("Вектор 2: " + v2);
+
+        Console.WriteLine("Сума: " + (v1 + v2));
+        Console.WriteLine("Різниця: " + (v1 - v2));
+        Console.WriteLine("Чи рівні? " + (v1 == v2));
+        Console.WriteLine("Чи різні? " + (v1 != v2));
+    }
+}
+
 }
